5e8a0112d7ae500562c68c471a09fa78a8cbf249
sravanthi9392-hub <polamsettiramu@gmail.com>
2026-06-14 18:16:28 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".typing", {
+
+    strings: [
+        "Computer Science Engineering Student"
+    ],
+
+    typeSpeed: 80,
+    backSpeed: 40,
+    loop: true
+
+});
+
+AOS.init({
+    duration:1200
+});
